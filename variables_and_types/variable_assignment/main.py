eb62b032840b8471c97d2384f299ce1ea38f743a
stzakis67-rgb <>
2026-06-02 19:23:52 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,10 @@
+#Reassigning the Variable
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+print("Eggs:", item_name)
+print("3.99:", item_price)
+print("120:", item_stock_quantity)
+print("In Stock:", in_stock)
